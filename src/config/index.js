32e50066dd50b42c843da8965c0861fd36ced8cc
Enis Mulić <enis.s.mulic@gmail.com>
2021-11-22 19:54:33 +0100

    feat(register): add user model for database

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,26 @@
-const config = {
-  database: {
-    host: process.env.DB_HOST,
-    name: process.env.DB_NAME,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    dialect: process.env.DB_DIALECT
-  }
+require("dotenv").config();
+
+const database = {
+  host: process.env.DB_HOST,
+  name: process.env.DB_NAME,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  dialect: process.env.DB_DIALECT
 };
 
-module.exports = config;
+const config = { database };
+module.exports = {
+  config,
+  development: {
+    ...database,
+    database: database.name
+  },
+  test: {
+    ...database,
+    database: database.name
+  },
+  production: {
+    ...database,
+    database: database.name
+  }
+};
