8a74b17f930f4ad7ba0a20859118599654b6d131
Enis Mulić <enis.s.mulic@gmail.com>
2021-11-22 22:15:31 +0100

    fix(registration): export router and rename endpoint

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const { authService } = require("../services/auth.service");
 
-router.post("/auth/register", async (req, res) => {
+router.post("/register", async (req, res) => {
   const user = await authService.register(req.body);
 
   return res.status(204).json({ user });
 });
+
+module.exports = router;
