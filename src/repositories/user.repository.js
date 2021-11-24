b48c4f381922d7e854da3b1354003569bc1559f2
Enis Mulić <enis.s.mulic@gmail.com>
2021-11-24 22:18:17 +0100

    feat(login): add getByEmail function to user repository

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -1,8 +1,13 @@
 const db = require("../database/models");
 
+const getByEmail = async (email) => {
+  const user = await db.User.findOne({ where: { email } });
+  return user;
+};
+
 const create = async (entity) => {
   const user = await db.User.create(entity);
   return user;
 };
 
-module.exports = { userRepository: { create } };
+module.exports = { userRepository: { create, getByEmail } };
