0229c0495067cf4b6d6dbb4a2a877c74f4d8b263
Enis Mulić <enis.s.mulic@gmail.com>
2021-11-24 20:24:32 +0100

    feat(register): add request validation for register endpoint

---
+++ src/validation/auth.validation.js
@@ -0,0 +1,12 @@
+const { body } = require("express-validator");
+
+const registerValidationRules = [
+  body("username").exists().withMessage("Username is a required field"),
+  body("email")
+    .exists()
+    .withMessage("Email is a required field")
+    .isEmail()
+    .withMessage("Value is not a valid email")
+];
+
+module.exports = { registerValidationRules };
