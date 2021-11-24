2f30227db5b4b0948b6db7c611c2b5b330c6ef2b
Lint Action <lint-action@samuelmeuli.com>
2021-11-24 21:21:54 +0000

    style: fix code style issues with Prettier

--- src/utility/jwt.js
+++ src/utility/jwt.js
@@ -1,7 +1,8 @@
 const jwt = require("jsonwebtoken");
 const { config } = require("../config");
 
-const generateJwt = (user) => jwt.sign(user, config.jwt.secret, {
+const generateJwt = (user) =>
+  jwt.sign(user, config.jwt.secret, {
     expiresIn: config.jwt.expiresIn
   });
 
