afe5a7eb9d142095b86bffa5b3a48d43b19836e7
Enis Mulić <enis.s.mulic@gmail.com>
2021-11-25 10:58:01 +0100

    feat(refresh-token): add jwt refresh token configuration

--- src/config/index.js
+++ src/config/index.js
@@ -10,7 +10,8 @@ const database = {
 
 const jwt = {
   secret: process.env.JWT_SECRET,
-  expiresIn: process.env.JWT_EXPIRES_IN
+  expiresIn: process.env.JWT_EXPIRES_IN,
+  refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN
 };
 
 const config = { database, jwt };
